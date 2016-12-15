c5015a049f9ab18d33e56d068a2543b7e6b08b67
turegjorup <ture@ignatz.dk>
2016-12-15 13:52:34 +0100

    ORG-34: Added Friluftsliv LineBased Resources

---
+++ src/AppBundle/Feed/FriluftslivHorseRidingTrailsReader.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: turegjorup
+ * Date: 24/08/16
+ * Time: 12:35
+ */
+
+namespace AppBundle\Feed;
+
+use Exception;
+use DateTime;
+use DateTimeZone;
+use stdClass;
+
+class FriluftslivHorseRidingTrailsReader extends BaseFriluftslivGeoJsonReader
+{
+  protected $feed_path = '/dataset/2eeec6af-09e1-4046-aea5-f4056f067ac7/resource/b2c04be4-4426-4c9c-8df7-9d5f2b2fb7b6/download/RideruterWGS84.json';
+
+  protected $id_string = 'horseridingtrail';
+  protected $type = 'horseridingtrails';
+  protected $origin_value = 'Horse Riding Trails from Friluftliv Aarhus';
+  protected $origin_url = 'https://www.odaa.dk/dataset/rideruter';
+
+}
---
+++ src/AppBundle/Feed/FriluftslivRunningTrailsReader.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: turegjorup
+ * Date: 24/08/16
+ * Time: 12:35
+ */
+
+namespace AppBundle\Feed;
+
+use Exception;
+use DateTime;
+use DateTimeZone;
+use stdClass;
+
+class FriluftslivRunningTrailsReader extends BaseFriluftslivGeoJsonReader
+{
+  protected $feed_path = '/dataset/1fa14692-c917-479d-b132-7b1979b20ea8/resource/7e8ca6cd-8454-49c5-adc2-a59ce511a7f4/download/Loeberuter2011WGS84.json';
+
+  protected $id_string = 'runningtrail';
+  protected $type = 'runningtrails';
+  protected $origin_value = 'Running Trails from Friluftliv Aarhus';
+  protected $origin_url = 'https://www.odaa.dk/dataset/loberuter';
+
+}
