c4f0da208d8fe8a9b365dc3a77999fd3b4146a9a
turegjorup <ture@ignatz.dk>
2016-12-15 15:10:12 +0100

    ORG-34: Added “jobs:delete” command

---
+++ src/AppBundle/Command/ClearJobQueuesCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Command;
+
+use AppBundle\Job\FriluftslivGearStationJob;
+use AppBundle\Job\FriluftslivFirepitsJob;
+use AppBundle\Job\Dokk1CountersJob;
+use AppBundle\Job\FriluftslivFitnessGymJob;
+use AppBundle\Job\RealTimeTrafficJob;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ClearJobQueuesCommand extends ContainerAwareCommand
+{
+  protected function configure()
+  {
+    $this
+      // the name of the command (the part after "bin/console")
+      ->setName('app:jobs:delete')
+      // the short description shown while running "php bin/console list"
+      ->setDescription('Delete All Jobs.')
+      // the full command description shown when running the command with
+      // the "--help" option
+      ->setHelp("This command delete all repeating jobs for CKANtoORION");
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output)
+  {
+    // get resque
+    $resque = $this->getContainer()->get('resque');
+    $resque->clearQueue('default');
+  }
+}
